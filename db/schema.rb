98091bde3980ec7e39b5867ee35b11857e6dd115
rebosuka <rebosukai@gmail.com>
2021-09-26 07:06:52 +0000

    user login remain

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_25_021617) do
+ActiveRecord::Schema.define(version: 2021_09_26_013506) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
